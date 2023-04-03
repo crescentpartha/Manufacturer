6eaa80dfd303617418aa4f963561f80a83289132
crescentpartha <crescentpartha406@gmail.com>
2023-04-04 00:09:15 +0600

    Setup more routes, _redirects file and title

--- manufacturer/src/App.js
+++ manufacturer/src/App.js
@@ -6,18 +6,22 @@ import Login from './components/Authentication/Login';
 import Register from './components/Authentication/Register';
 import RetrievePassword from './components/Authentication/RetrievePassword';
 import NotFound from './components/Shared/NotFound';
+import Header from './components/Shared/Header';
+import Footer from './components/Shared/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/retrieve-password' element={<RetrievePassword></RetrievePassword>}></Route>
-        <Route path='/notfound' element={<NotFound></NotFound>}></Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
