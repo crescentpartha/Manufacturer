ebd14e44fe2c792e611a24f5c6a3164bdd73eec2
crescentpartha <crescentpartha406@gmail.com>
2023-04-03 23:59:55 +0600

    Setup routes

--- manufacturer/src/App.js
+++ manufacturer/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Authentication/Login';
+import Register from './components/Authentication/Register';
+import RetrievePassword from './components/Authentication/RetrievePassword';
+import NotFound from './components/Shared/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/retrieve-password' element={<RetrievePassword></RetrievePassword>}></Route>
+        <Route path='/notfound' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ manufacturer/src/components/Authentication/RetrievePassword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RetrievePassword = () => {
+    return (
+        <div>
+            <h2>Forget Password</h2>
+        </div>
+    );
+};
+
+export default RetrievePassword;
